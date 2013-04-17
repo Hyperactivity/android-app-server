0572f183c96401cec6da889afe161b2feeced3f9
OMMatte <ommatte@gmail.com>
2013-04-17 14:06:37 +0200

    Fixed createReply and added a sorting function for replies

--- src/models/Thread.java
+++ src/models/Thread.java
@@ -1,6 +1,7 @@
 package models;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.List;
 
 /**
@@ -10,7 +11,7 @@ import java.util.List;
  * Time: 13:14
  */
 @Entity
-public class Thread {
+public class Thread implements Serializable {
 
     public Thread(Category parentCategory, Account account, String threadName, String threadText)
     {

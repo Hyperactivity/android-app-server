9283328d8235edb9489b85457c5981027d1f2691
OMMatte <ommatte@gmail.com>
2013-04-22 10:49:04 +0200

    Small fix with content

--- src/assistant/Constants.java
+++ src/assistant/Constants.java
@@ -27,9 +27,9 @@ public abstract class Constants {
         public static final String LOGIN = "login";
         public static final String GET_PROFILE = "get_profile";
         public static final String UPDATE_PROFILE = "update_profile";
-        public static final String GET_FORUM = "get_forum";
-        public static final String GET_CATEGORY = "get_category";
-        public static final String GET_THREAD = "get_thread";
+        public static final String GET_FORUM = "get_forum_content";
+        public static final String GET_CATEGORY = "get_category_content";
+        public static final String GET_THREAD = "get_thread_content";
         public static final String CREATE_THREAD = "create_thread";
         public static final String CREATE_REPLY = "create_reply";
     }

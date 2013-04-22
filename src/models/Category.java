b22ba801054c3a6c5bbe05435134f62680c72ff9
OMMatte <ommatte@gmail.com>
2013-04-22 11:25:53 +0200

    Fixed Category.find

---
+++ src/models/Category.java
@@ -0,0 +1,105 @@
+package models;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: OMMatte
+ * Date: 2013-04-22
+ * Time: 11:05
+ */
+@Entity
+public class Category {
+    private int id;
+
+    public Category(int id, String headLine, int colorCode, Category parentCategory) {
+        this.id = id;
+        this.headLine = headLine;
+        this.colorCode = colorCode;
+        this.parentCategory = parentCategory;
+    }
+
+    @Deprecated
+    public Category() {
+    }
+
+    @javax.persistence.Column(name = "id")
+    @Id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private String headLine;
+
+    @javax.persistence.Column(name = "headLine")
+    @Basic
+    public String getHeadLine() {
+        return headLine;
+    }
+
+    public void setHeadLine(String headLine) {
+        this.headLine = headLine;
+    }
+
+    private int colorCode;
+
+    @javax.persistence.Column(name = "colorCode")
+    @Basic
+    public int getColorCode() {
+        return colorCode;
+    }
+
+    public void setColorCode(int colorCode) {
+        this.colorCode = colorCode;
+    }
+
+    private Category parentCategory;
+
+    @ManyToOne(optional=true, fetch=FetchType.LAZY)
+    @JoinColumn(name="parentCategoryId")
+    public Category getParentCategory() {
+        return parentCategory;
+    }
+
+    public void setParentCategory(Category parentCategory) {
+        this.parentCategory = parentCategory;
+    }
+
+    private List<Category> childCategories;
+
+    @OneToMany(mappedBy="parentCategory")
+    public List<Category> getChildCategories() {
+        return childCategories;
+    }
+
+    public void setChildCategories(List<Category> childCategories) {
+        this.childCategories = childCategories;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        if (colorCode != category.colorCode) return false;
+        if (id != category.id) return false;
+        if (headLine != null ? !headLine.equals(category.headLine) : category.headLine != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (headLine != null ? headLine.hashCode() : 0);
+        result = 31 * result + colorCode;
+        return result;
+    }
+}
